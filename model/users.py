2a62fa8d3797c9ff401c5a037b7eb0955335a3e4
LincolnC2008 <142536112+LincolnC2008@users.noreply.github.com>
2024-01-30 10:23:58 -0800

    chamge

--- model/users.py
+++ model/users.py
@@ -223,3 +223,4 @@ def initUsers():
                 '''fails with bad or duplicate data'''
                 db.session.remove()
                 print(f"Records exist, duplicate email, or error: {user.uid}")
+                #change
