5b3a139aca4e0d804bbb0ab09bed7a0674edc5a9
Ashwinv93 <ashwinkrv@gmail.com>
2024-03-18 19:13:04 +0000

    titanic

--- main.py
+++ main.py
@@ -15,10 +15,13 @@ from __init__ import app, db, cors
 from api.user import user_api  # Blueprint import api definition
 from api.clashRoyal import cards_api
 from api.player import player_api
+from api.titanic import titanic_api
 # database migrations
 from model.users import initUsers
 from model.players import initPlayers
 from model.clashroyal import initCards
+from model.titanics import initTitanic
+
 
 # setup App pages
 # Blueprint directory import projects definition
@@ -34,6 +37,8 @@ app.register_blueprint(user_api)  # register api routes
 app.register_blueprint(player_api)
 app.register_blueprint(app_projects)  # register app pages
 app.register_blueprint(cards_api)
+app.register_blueprint(titanic_api)
+
 
 @app.errorhandler(404)  # catch for URL not found
 def page_not_found(e):
@@ -90,6 +95,7 @@ def generate_data():
     initUsers()
     initCards()
     initPlayers()
+    initTitanic()
 
 
 # Register the custom command group with the Flask application
