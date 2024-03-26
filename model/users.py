8f8765bcd4c5e8298ea11f3690e519ba10c21b89
Ashwinv93 <ashwinkrv@gmail.com>
2024-03-26 04:39:58 +0000

    changed default users

--- model/users.py
+++ model/users.py
@@ -220,10 +220,10 @@ def initUsers():
         """Create database and tables"""
         db.create_all()
         """Tester data for table"""
-        u1 = User(name='Thomas Edison', uid='toby', password='123toby', dob=date(1847, 2, 11), role="Admin")
-        u2 = User(name='Nicholas Tesla', uid='niko', password='123niko', dob=date(1856, 7, 10))
-        u3 = User(name='Alexander Graham Bell', uid='lex')
-        u4 = User(name='Grace Hopper', uid='hop', password='123hop', dob=date(1906, 12, 9))
+        u1 = User(name='Ashwin', uid='Ashwin', password='Ashwin', dob=date(2008, 4, 7), role="Admin")
+        u2 = User(name='Dante', uid='Dante', password='Dante', dob=date(1856, 7, 10), role="Admin")
+        u3 = User(name='Vance', uid='Vance', password='Vance', dob=date(2008, 4, 7), role="Admin")
+        u4 = User(name='Hog Rider', uid='Hog', password='Hog', dob=date(1906, 12, 9), role="Admin")
         users = [u1, u2, u3, u4]
 
         """Builds sample user/note(s) data"""
